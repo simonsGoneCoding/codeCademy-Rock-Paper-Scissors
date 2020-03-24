9e8ee12f28a76c45ce3a850d05aa36774a4c7a1a
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-03-24 19:25:21 +0000

    Created all logic to determine and print out the winner.

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const getUserChoice = userInput => {
   if (
     userInput === "rock" ||
     userInput === "paper" ||
-    userInput === "scissors"
+    userInput === "scissors" ||
+    userInput === "bomb"
   ) {
     return userInput;
   } else {
-    console.log("invalid input");
+    console.log("invalid user input");
   }
 };
 
-const getComputerInput = () => {
+const getComputerChoice = () => {
   switch (Math.floor(Math.random() * 3)) {
     case 0:
       return "paper";
@@ -26,3 +27,45 @@ const getComputerInput = () => {
       break;
   }
 };
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === "bomb") {
+    return "you win";
+  }
+
+  if (userChoice === computerChoice) {
+    return "draw";
+  }
+
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "you lose";
+    } else if (computerChoice === "scissors") {
+      return "you win";
+    }
+  }
+
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "you lose";
+    } else if (computerChoice === "rock") {
+      return "you win";
+    }
+  }
+
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "you lose";
+    } else if (computerChoice === "paper") {
+      return "you win";
+    }
+  }
+};
+
+const playGame = userInput => {
+  userChoice = getUserChoice(userInput);
+  computerChoice = getComputerChoice();
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame("scissors");
